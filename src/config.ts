89e450e232e4ec498dc9a878f8f0fbd101fffc84
ccousensdanvers <ccousens@danversma.gov>
2025-09-09 07:35:43 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,12 @@
 
 export const config = {
-  universe: ['AAPL', 'MSFT', 'NVDA', 'GOOGL', 'META'],
+  universe: [
+  'NVDA','AMD','TSLA','AAPL','MSFT','GOOG','AMZN','META','PLTR','NEE',
+  'CRWD','V','PYPL','UNH','AVGO','DOCU','ORCL','NVCR','ARWR','ADBE',
+  'PANW','NFLX','SNOW','SHOP','NET','ZS','MDB','SMCI','UBER','LYFT','COIN',
+  'MSTR'
+]
+,
   weights: {
     trend: 30,
     momentum: 20,
